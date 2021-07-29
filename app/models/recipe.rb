2f0f38127b7ac7b05f3008d755b282fb42d515f2
flatlandnick <nicholas.lunn@flatironschool.com>
2021-07-29 14:03:37 -0500

    fingers crossed

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,8 +1,11 @@
 class Recipe < ApplicationRecord
-    belongs_to :user, class_name: "creator"
+    belongs_to :user
+
+    has_many :reviews
+    has_many :users, through: :reviews
 
     has_many :user_recipes
-    has_many :users, through: :user_recipes, class_name: "User"
+    has_many :recipe_users, through: :user_recipes, class_name: "User"
 
     has_many :recipe_ingredients
     has_many :ingredients, through: :recipe_categories
